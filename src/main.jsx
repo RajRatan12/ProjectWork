b4d1bc6a877d5451cbd2cd89261fb619b17e06e1
Debangshu_Saha <sahdebsaha909@gmail.com>
2024-01-21 23:18:57 +0530

    Added Routes for the footer elements

--- src/main.jsx
+++ src/main.jsx
@@ -5,6 +5,10 @@ import Layout from './Layout.jsx'
 import { createBrowserRouter,createRoutesFromElements,Route, RouterProvider } from 'react-router-dom'
 
 import {Home,About,Services} from "./components/index.jsx"
+import FooterAbout from "./components/FooterComponents/FooterAbout.jsx"
+import FooterContact from "./components/FooterComponents/FooterContact.jsx"
+import FooterKnowTheTeam  from "./components/FooterComponents/FooterKnowTheTeam.jsx"
+import FooterReference from './components/FooterComponents/FooterAbout.jsx'
 
 const router=createBrowserRouter(
   createRoutesFromElements(
@@ -12,6 +16,10 @@ const router=createBrowserRouter(
       <Route path="" element={<Home/>}/>
       <Route path="about" element={<About/>}/>
       <Route path="services" element={<Services/>}/>
+      <Route path="footer-about" element={<FooterAbout/>}/>
+      <Route path="footer-contact" element={<FooterContact/>}/>
+      <Route path="footer-KnowTheTeam" element={<FooterKnowTheTeam/>}/>
+      <Route path="footer-reference" element={<FooterReference/>}/>
     </Route>
   )
 )
