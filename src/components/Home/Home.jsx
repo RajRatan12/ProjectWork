85862a2ec2e790de66bc11e79425e01d7baace0d
RajRatan12 <rajratancse@gmail.com>
2024-01-21 19:03:29 +0530

    from raj

--- src/components/Home/Home.jsx
+++ src/components/Home/Home.jsx
@@ -4,7 +4,9 @@ const Home = () => {
   return (
     <div>
     <p>From Debangshu</p>
-    <p className='text-center text-3xl bg-green-300'>Home Page</p></div>
+    <p className='text-center text-3xl bg-green-300'>Home Page</p>
+    <p>from raj</p>
+    </div>
   )
 }
 
