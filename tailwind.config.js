856340abed5dcd975799018c7cff1070cd0437f2
RajRatan12 <rajratancse@gmail.com>
2024-01-21 20:46:44 +0530

    Added Header and Footer

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,12 +1,20 @@
 /** @type {import('tailwindcss').Config} */
 export default {
+  daisyui: {
+    themes: ["light", "dark", "cupcake"],
+  },
   content: [
     "./index.html",
     "./src/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      fontFamily: {
+        Rajdhani: ["Rajdhani"],
+      },
+    },
   },
-  plugins: [],
+  plugins: [
+    require("daisyui")],
 }
 
