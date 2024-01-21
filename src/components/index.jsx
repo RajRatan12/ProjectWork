1b2dc0cf2361c019e0d4a30dd8cd2b8489c5bde9
Debangshu_Saha <sahdebsaha909@gmail.com>
2024-01-21 23:18:57 +0530

    Added Routes for the footer elements

--- src/components/index.jsx
+++ src/components/index.jsx
@@ -1,7 +1,18 @@
 import Home from "./Home/Home"
-
 import About from "./About/About"
-
 import Services from "./Services/Services"
 
-export {Home,About,Services}
+import FooterAbout from "./FooterComponents/footerAbout"
+import FooterContact from "./FooterComponents/footerContact"
+import FooterReference from "./FooterComponents/footerReference"
+import FooterKnowTheTeam from "./FooterComponents/footerKnowTheTeam"
+
+export {
+    Home,
+    About,
+    Services,
+    FooterAbout,
+    FooterContact,
+    FooterReference,
+    FooterKnowTheTeam
+}
