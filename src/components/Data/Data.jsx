6ce2922dea79a34b41f43ba290fbd5ca46e08322
RajRatan12 <rajratancse@gmail.com>
2024-01-22 17:30:19 +0530

    Landing Page Progress Part 2

---
+++ src/components/Data/Data.jsx
@@ -0,0 +1,34 @@
+export const data = [
+    {
+      id: 1,
+      name: "Earthquake",
+      description: "earthquake",
+      image:
+      "https://images.pexels.com/photos/15803784/pexels-photo-15803784/free-photo-of-ruins-of-buildings-in-city.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2",
+      // price: "$$$$",
+    },
+    {
+      id: 2,
+      name: "Flood",
+      description: "flood",
+      image:
+        "https://images.pexels.com/photos/6471946/pexels-photo-6471946.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2",
+      // price: "$",
+    },
+    {
+      id: 3,
+      name: "Wildfire",
+      description: "wildfire",
+      image:
+        "https://images.pexels.com/photos/51951/forest-fire-fire-smoke-conservation-51951.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2",
+      // price: "$$",
+    },
+    {
+      id: 4,
+      name: "Cyclone",
+      description: "cyclone",
+      image:
+        "https://images.pexels.com/photos/5681374/pexels-photo-5681374.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2",
+      // price: "$$$",
+    },
+  ];
