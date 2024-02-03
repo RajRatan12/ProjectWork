88b13a02564050d286859fd36eaaa21ed00a1a74
Debangshu_Saha <sahdebsaha909@gmail.com>
2024-02-03 22:03:14 +0530

    Updated the About Us Webpage with proper margin and padding

--- FrontEnd/src/components/About/About.jsx
+++ FrontEnd/src/components/About/About.jsx
@@ -4,20 +4,20 @@ import Aboutimg3 from '../../Assets/Aboutimg3.jpg'
 
 const About = () => {
   return (
-    <div className="space-y-6" style={{ 
+    <div className="space-y-6 md:space-y-0" style={{ 
       backgroundImage: `url(${Aboutimg3})`,
       backgroundSize: 'cover',  
       backgroundRepeat: 'no-repeat',  
       backgroundPosition: 'center center',  
     }}>
-      <div className="flex flex-wrap justify-center space-y-6 md:space-y-0 md:space-x-[5rem]">
+      <div className="flex flex-wrap justify-center space-y-12 md:space-y-0 md:space-x-[5rem]">
         {ProfileCardData.slice(0, 3).map((profile, index) => (
-          <div key={index} className="w-full md:w-1/3 lg:w-1/4 xl:w-1/5">
+          <div key={index} className=" w-full md:w-1/3 lg:w-1/4 xl:w-1/5">
             <ProfileCards {...profile} />
           </div>
         ))}
       </div>
-      <div className="flex flex-wrap justify-center space-y-6 md:space-y-0 md:space-x-[5rem]">
+      <div className="flex flex-wrap justify-center space-y-12 md:space-y-0 md:space-x-[5rem]">
         {ProfileCardData.slice(3, 5).map((profile, index) => (
           <div key={index} className="w-full md:w-1/3 lg:w-1/4 xl:w-1/5">
             <ProfileCards {...profile} />
