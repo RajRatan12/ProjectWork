f8da8e45b039a06a5864af0a2ceb0f406286433c
RajRatan12 <rajratancse@gmail.com>
2024-03-17 23:53:25 +0530

    home  page card updated

--- FrontEnd/src/components/Data/Data.jsx
+++ FrontEnd/src/components/Data/Data.jsx
@@ -2,7 +2,8 @@ export const data = [
     {
       id: 1,
       name: "Earthquake",
-      description: "earthquake",
+      description1: "earthquake",
+      description2: "1)Drop to the ground to avoid being knocked over. 2)Take cover under a sturdy piece of furniture or against an interior wall. 3) Hold on until shaking stops to prevent injury. 4) Stay indoors until it's safe to exit, avoiding elevators. If outside, move to an open area away from buildings, trees, and utility wires.  ",
       image:
       "https://images.pexels.com/photos/15803784/pexels-photo-15803784/free-photo-of-ruins-of-buildings-in-city.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2",
       // price: "$$$$",
@@ -10,7 +11,8 @@ export const data = [
     {
       id: 2,
       name: "Flood",
-      description: "flood",
+      description1: "flood",
+      description2: "1)Evacuate to higher ground immediately. 2)Avoid walking or driving through floodwaters. 3)Avoid walking or driving through floodwaters. 4)Follow evacuation orders and seek shelter in designated areas. 5)Stay informed through local news and emergency alerts for updates. ",
       image:
         "https://images.pexels.com/photos/6471946/pexels-photo-6471946.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2",
       // price: "$",
@@ -18,7 +20,8 @@ export const data = [
     {
       id: 3,
       name: "Wildfire",
-      description: "wildfire",
+      description1: "wildfire",
+      description2: "1)Evacuate to a safe location if instructed by authorities. 2)Close all windows and doors to prevent embers from entering. 3)Pack essential items like medications, documents, and valuables. 4)Pack essential items like medications, documents, and valuables. 5)Stay low to the ground if trapped in smoke. 6)Follow evacuation routes and avoid blocked areas. ",
       image:
         "https://images.pexels.com/photos/51951/forest-fire-fire-smoke-conservation-51951.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2",
       // price: "$$",
--- FrontEnd/src/components/Home/Home.jsx
+++ FrontEnd/src/components/Home/Home.jsx
@@ -1,10 +1,8 @@
-// import React from 'react'
-import { useState, useRef, useEffect } from "react";
-import Landing_Page1 from "../../Assets/Landing_Page1.jpg";
-// import Landing_Page2 from '../../Assets/Landing_Page2.jpg'
-import pexels from "../../Assets/pexels-eberhard-grossgasteiger-1292115.jpg";
-import { data } from "../Data/Data";
-import { IoCloseCircleSharp } from "../../Assets/Icons";
+import React, { useState, useRef, useEffect } from 'react';
+import Landing_Page1 from '../../Assets/Landing_Page1.jpg';
+import pexels from '../../Assets/pexels-eberhard-grossgasteiger-1292115.jpg';
+import { data } from '../Data/Data';
+import { IoCloseCircleSharp } from '../../Assets/Icons';
 
 export function useIsVisible(ref) {
   const [isIntersecting, setIntersecting] = useState(false);
@@ -27,50 +25,48 @@ function Home() {
   const refs = Array.from({ length: 3 }, () => useRef());
   const isVisible = refs.map((ref) => useIsVisible(ref));
   const [fade, setFade] = useState(false);
+  const [selectedDisasterIndex, setSelectedDisasterIndex] = useState(null);
 
-  const onBringGlassInfront = () => {
+  const onBringGlassInfront = (index) => {
     setFade(!fade);
+    setSelectedDisasterIndex(index);
   };
 
   return (
     <div className="">
       <div
-        className=" w-[100%] h-screen flex-col flex justify-center mx-auto max-w-full bg-cover bg-fixed bg-center bg-no-repeat shadow-lg "
+        className="w-[100%] h-screen flex-col flex justify-center mx-auto max-w-full bg-cover bg-fixed bg-center bg-no-repeat shadow-lg "
         style={{ backgroundImage: `url(${Landing_Page1})` }}
       >
-        <div className=" ml-[3%]  p-10 text-black m-auto md:w-[700px] ">
-          <p className="Rajdhani font-bold text-3xl">
-            Welcome to ThreatSenseAI
-          </p>
-          <p className="p-2 Rajdhani">
-            ahjkhsoiajsdjnasdlhlais dauby ak hxku asugxukxgasg b
-          </p>
+        <div className="ml-[3%]  p-10 text-black m-auto md:w-[700px] ">
+          <p className="Rajdhani font-bold text-3xl">Welcome to ThreatSenseAI</p>
+          <p className="p-2 Rajdhani">ahjkhsoiajsdjnasdlhlais dauby ak hxku asugxukxgasg b</p>
         </div>
       </div>
 
       <div
-        className=" w-[100%]  flex-col flex justify-center mx-auto max-w-full bg-cover bg-fixed bg-center bg-no-repeat shadow-lg"
+        className="w-[100%] flex-col flex justify-center mx-auto max-w-full bg-cover bg-fixed bg-center bg-no-repeat shadow-lg"
         style={{ backgroundImage: `url(${pexels})` }}
       >
         <div className="grid sm:grid-cols-2 md:grid-cols-3 m-auto gap-x-5 gap-y-5 mt-8 p-3 ml-3 mr-3">
           {data.map((item, index) => (
             <div
               ref={refs[index]}
-              className={` transition-opacity ease-in duration-700 card md:w-90 sm:w-70 glass m-3 ${
-                isVisible[index] ? "opacity-100" : "opacity-0"
+              className={`transition-opacity ease-in duration-700 card md:w-90 sm:w-70 glass m-3 ${
+                isVisible[index] ? 'opacity-100' : 'opacity-0'
               }`}
               key={index}
             >
               <figure>
                 <img src={item.image} alt={item.name} />
               </figure>
-              <div className={` card-body`}>
+              <div className={`card-body`}>
                 <h2 className="card-title">{item.name}</h2>
-                <p>{item.description}</p>
+                {/* <p>{item.description1}</p> */}
                 <div className="card-actions justify-end">
                   <button
                     className="btn btn-primary transition ease-in-out delay-200 hover:-translate-y-1 hover:scale-110 duration-250"
-                    onClick={onBringGlassInfront}
+                    onClick={() => onBringGlassInfront(index)}
                   >
                     Learn now!
                   </button>
@@ -83,35 +79,30 @@ function Home() {
         <div>
           <div className="flex justify-center mb-[4%]">
             <div
-              className={`${fade === true ? "" : "hidden"} card w-[65%] glass`}
+              className={`${fade === true && selectedDisasterIndex !== null ? '' : 'hidden'} card w-[65%] glass`}
             >
               <div className="card-body">
                 <div className="flex justify-end">
                   <button
                     type="button"
                     className="glass rounded-2xl  inline-flex items-center justify-center text-gray-400 hover:text-gray-500 hover:bg-gray-100 focus:outline-none "
-                    onClick={onBringGlassInfront}
+                    onClick={() => {
+                      setFade(false);
+                      setSelectedDisasterIndex(null);
+                    }}
                   >
                     <span className="sr-only">Close menu</span>
-                    <svg
-                      className="h-6 w-6"
-                      xmlns="http://www.w3.org/2000/svg"
-                      fill="none"
-                      viewBox="0 0 24 24"
-                      stroke="currentColor"
-                      aria-hidden="true"
-                    >
-                      <path
-                        strokeLinecap="round"
-                        strokeLinejoin="round"
-                        strokeWidth="2"
-                        d="M6 18L18 6M6 6l12 12"
-                      />
-                    </svg>
+                    <IoCloseCircleSharp className="h-6 w-6" />
                   </button>
                 </div>
-                <h2 className="card-title">Life hack</h2>
-                <p>How to park your car at your garage?</p>
+                <div className="">
+                  {selectedDisasterIndex !== null && (
+                    <div  >
+                      <h2  >{data[selectedDisasterIndex].name}</h2>
+                      <p>{data[selectedDisasterIndex].description2}</p>
+                    </div>
+                  )}
+                </div>
               </div>
             </div>
           </div>
