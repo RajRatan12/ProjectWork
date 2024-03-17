4647cca1b17357e68705c420aaff525cc67d4439
Debangshu_Saha <sahdebsaha909@gmail.com>
2024-03-17 13:32:11 +0530

    Video changed

--- FrontEnd/src/components/About/About.jsx
+++ FrontEnd/src/components/About/About.jsx
@@ -1,5 +1,5 @@
 import React from 'react'
-import video1 from '../../Assets/video1.mp4'
+import videobg from '../../Assets/videobg.mp4'
 const About = () => {
   return (
     <div className=''>
@@ -7,7 +7,7 @@ const About = () => {
       </div>{" "}
       <div className=" m-0 p-0 w-[100%] h-[100vh] ">
         <video
-          src={video1}
+          src={videobg}
           autoPlay
           loop
           muted
